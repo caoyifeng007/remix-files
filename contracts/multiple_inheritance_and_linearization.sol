93975acd46bab522a14721bd704ac15e79c260ca
ElvinsJ <caoyifeng007@163.com>
2023-02-14 10:55:21 +0800

    多重继承 c3 linearization会按照继承顺序来执行构造函数以及向上查找继承的方法

--- contracts/multiple_inheritance_and_linearization.sol
+++ contracts/multiple_inheritance_and_linearization.sol
@@ -21,6 +21,8 @@ pragma solidity ^0.8.0;
 // }
 
 contract A {
+    uint256 public x;
+    
     function sayHi() public pure virtual returns(string memory) {
         return "hello A";
     }
@@ -33,6 +35,67 @@ contract B {
 
 contract C is A, B {
     function sayHi() public pure override(A, B) returns(string memory) {
+        // "hello B"
         return super.sayHi();
     }
+
+    function setX() external {
+        x = 1;
+    }
+}
+
+contract D {
+    function sayHi() public pure virtual returns(string memory) {
+        return "hello D";
+    }
+}
+contract E is D {
+    function sayHi() public pure virtual override(D) returns(string memory) {
+        return "hello E";
+    }
+}
+
+contract F is D {
+    function sayHi() public pure virtual override(D) returns(string memory) {
+        // 由于c3 linearization的关系,会返回"hello E",并不是"hello D"
+        return super.sayHi();
+    }
+}
+
+contract G is E,F {
+    function sayHi() public pure override(E,F) returns(string memory) {
+        return super.sayHi();
+    }
+}
+
+contract Base1 {
+    constructor() {}
+}
+
+contract Base2 {
+    constructor() {}
+}
+
+// Constructors are executed in the following order:
+//  1 - Base1
+//  2 - Base2
+//  3 - Derived1
+contract Derived1 is Base1, Base2 {
+    constructor() Base1() Base2() {}
+}
+
+// Constructors are executed in the following order:
+//  1 - Base2
+//  2 - Base1
+//  3 - Derived2
+contract Derived2 is Base2, Base1 {
+    constructor() Base2() Base1() {}
+}
+
+// Constructors are still executed in the following order:
+//  1 - Base2
+//  2 - Base1
+//  3 - Derived3
+contract Derived3 is Base2, Base1 {
+    constructor() Base1() Base2() {}
 }
